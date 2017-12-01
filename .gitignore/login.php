18fa189d7ce55e17dc3d5c7bb218f8d60dbc0ec5
Pablo10499 <34157603+Pablo10499@users.noreply.github.com>
2017-12-01 12:02:33 +0100

    Create login.php

---
+++ .gitignore/login.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title> CONNECTION</title>
+	</head>
+	<style>
+	h1 {
+		text-align: center;
+		font-size: 30px;
+		font-family: verdana;
+	}
+	
+	body {
+		text-align: center;
+		padding: center;
+		margin: center;
+	}
+	
+	
+	table
+{
+margin: auto;
+
+}
+td
+{
+text-align :auto;
+
+
+
+}
+
+	
+	
+	</style>
+		
+		<body>
+			
+				 <form method="post">
+				 
+			<h1><b>Renseignez vos identifiants</b><h1>
+			
+			<table style="width:50%">
+		
+			<tr style="auto">
+			
+			
+			
+				<td>
+					<input type="email" placeholder="email" id="Mail" name="mail" style="width:50%">
+					
+				
+					
+					
+				</td>
+			</tr>
+			
+			<tr> 	
+			
+			
+				<td>
+					<input type="password" placeholder=" Mot de passe" id="mdp" name="mot de passe" style="width:50%";>
+				</td>
+			</tr>
+			
+			
+			</table>
+			</br>
+				<input type="checkbox" name="forminscription" value="Se souvenir de moi" /> 
+				</br>
+				<input type="submit" value="Connexion"/>
+				
+			</form>
